c1bdd704366492c5e6a03ff2a724d61194a7d2df
Michael Noseworthy <michael.noseworthy@georgebrown.ca>
2020-12-02 21:13:20 -0500

    Added CLI
    Added while loop and case switches as well as sentinals to the UI.

--- Group35Assignment.sh
+++ Group35Assignment.sh
@@ -1,43 +1,55 @@
 #!/bin/bash
 
-@echo test
+echo "Welcome.  This is assignment 3 for group 35 which includes Emma Kule, Jennifer Wrigglesworth and Michael Noseworthy."
 
-method = 'M'
 
-echo -n "Enter the name of a country: "
-read method
 
-echo -n "The official language of $method is "
 
-case $method in
-S | s)
-read -p "Enter value for a " a
-read -p "Enter value for b " b
-read -p "Enter value for sequence " n
-if [[ n -le 0 ]]
-then
-echo "ERROR- sequence must be greater than 0"
-else
-for ((i=1; i<=n; i++))
+while true;
 do
-x=$(($i * $a))
-w=$(($x + $b))
-echo $w
-;;
-  Romania | Moldova)
-    echo -n "Romanian"
-    ;;
+	echo "Please enter which method you want to use.  M for task 1, P for task 2 or S for task 3: "
+	read METHOD
 
-  Italy | "San Marino" | Switzerland | "Vatican City")
-    echo -n "Italian"
-    ;;
+	case $METHOD in
 
-  *)
-    echo -n "unknown"
-    ;;
-esac
+		M | m)
+			echo "Task 1"
+			#TODO
+			read test
+			;;
 
-
-
+		P | p)
+			echo "Task 2"
+			#TODO
+			;;
+		
+		S | s)
+			echo "Task 3"
+			read -p "Enter value for a " a
+			read -p "Enter value for b " b
+			read -p "Enter value for sequence " n
+			if [[ n -le 0 ]]
+			then
+			echo "ERROR- sequence must be greater than 0"
+			else
+			for ((i=1; i<=n; i++))
+			do
+			x=$(($i * $a))
+			w=$(($x + $b))
+			echo $w
+			done
+			fi
+			;;
+			
+		Q | q)
+			echo "Thanks for examining our assignment.  Good bye."
+			exit
+			;;
+			
+		*)
+			echo -n "Wrong sentinel selected.  Please try again or press Q for exit."
+			;;
+			
+	esac
+#place while loop ending
 done
-fi 
