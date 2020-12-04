ba76431a77ccb71c1ec140161634ef94f6dd6713
Michael Noseworthy <michael.noseworthy@georgebrown.ca>
2020-12-03 23:38:00 -0500

    Task 1 completed and more UI added
    Added task 1 code.  Updated UI to look more professional.

--- Group_35_Application.sh
+++ Group_35_Application.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 
-echo "Welcome.  This is assignment 3 for group 35 which includes Emma Kule, Jennifer Wrigglesworth and Michael Noseworthy."
+User1="Emma Kule"
+User2="Jennifer Wrigglesworth"
+User3="Michael Noseworthy"
 
+echo "Welcome.  This is assignment 3 for group 35 for the course COMP 1223 (Web Development Fundamentals) and assembled by: "
+printf "$User1, $User2 and $User3.\n"
 
-
-
-while true;
+while true; #do forever until user enters a false value
 do
-	echo "Please enter which method you want to use.  M for task 1, P for task 2 or S for task 3: "
-	read METHOD
+	echo "Please enter which method you want to use.  Press Q to quit, M for task 1, S for task 2 or P for task 3"
+	read METHOD #Tells the program which task to go to
 
 	case $METHOD in
 
 		M | m)
-			echo "Task 1"
-			#TODO
-			read test
+			echo "Task 1: Find the even multiples of any number – the user should specify the multiples of which
+number they want to print and how many of them."
+			#Bash Script to check whether a number is even or odd
+			read -p "Enter your number to test: " NUMBER
+			read -p "Enter how many numbers you want to display: " MULTI
+			printf "You are testing the number $NUMBER and you want to display $MULTI values.\n"
+			echo "Displaying even numbers: "
+			INDEX=1 #set to 1 to prevent 0 as a number to test
+			while [ $INDEX -lt $NUMBER ]
+			do
+					if [ $MULTI == 0 ] #Check to see if there are any more numbers to display
+					then
+					break #End if number of display numbers is finished
+					fi
+			REMAINDER=$(( $INDEX % 2 )) #Set the remainder to a boolean
+				if [ $REMAINDER -eq 0 ] #Check to see if the number is odd or even
+				
+				then
+				echo $INDEX #output number if the number is odd
+				MULTI=$(($MULTI-1)) #Tells the program that a number was displayed and deincrement
+				fi
+				INDEX=$(($INDEX+1)) #increase index for next loop
+			done
+			echo "Task 1 complete.  Returning to main menu"
 			;;
-
-		P | p)
-			echo "Task 2"
-			#TODO
-			;;
-		
+			
 		S | s)
-			echo "Task 3"
+			echo "Task 2: Find the terms of any linear sequence given by the rule Un = an + b, where a and b are
+integers specified by the user and n is a positive integer, and print them in increasing order (for
+example if the user inputs a=3, b=-4, the first few terms that should be printed are -1, 2, 5, 8,
+11…)."
 			read -p "Enter value for a " a
 			read -p "Enter value for b " b
 			read -p "Enter value for sequence " n
@@ -39,7 +60,18 @@ do
 			echo $w
 			done
 			fi
+			echo "Task 2 complete.  Returning to main menu"
 			;;
+
+		P | p)
+			echo "Task 3: Find the numbers that can be expressed as the product of two nonnegative integers in
+succession and print them in increasing order. (For example, 30 is such a number as it can be
+written as the product of 5 and 6 (30=5x6), which are two numbers in succession.)"
+			#TODO
+			echo "Task 3 complete.  Returning to main menu"
+			;;
+		
+		
 			
 		Q | q)
 			echo "Thanks for examining our assignment.  Good bye."
@@ -47,9 +79,9 @@ do
 			;;
 			
 		*)
-			echo -n "Wrong sentinel selected.  Please try again or press Q for exit."
+			printf "Wrong sentinel selected.  Please try again or press Q for exit.\n"
 			;;
 			
-	esac
+	esac #Exit of switch loop
 #place while loop ending
 done
